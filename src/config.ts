7187dc77f3538a93bedc6c495b6ead19977f0b7e
nakorchy <455965893@qq.com>
2026-02-23 21:22:31 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -22,8 +22,8 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
-	subtitle: "One demo website",
+	title: "沂夏",
+	subtitle: "一个分享、创作网站",
 	siteURL: "https://blog.nakorchy.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-12-22", // 站点开始运行日期，用于站点统计组件计算运行天数
 
@@ -57,7 +57,7 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/home.png",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.png",
+		logo: "assets/home/logo.png",
 	},
 
 	// 页面自动缩放配置
