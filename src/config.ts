501dd4d31d236fa56856dae4786a0983c62c879d
nakorchy <455965893@qq.com>
2026-04-22 21:59:09 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -38,12 +38,12 @@ export const siteConfig: SiteConfig = {
 	// 特色页面开关配置(关闭不在使用的页面有助于提升SEO,关闭后直接在顶部导航删除对应的页面就行)
 	featurePages: {
 		anime: false, // 番剧页面开关
-		diary: true, // 日记页面开关
+		diary: false, // 日记页面开关
 		friends: true, // 友链页面开关
 		projects: false, // 项目页面开关
 		skills: true, // 技能页面开关
 		timeline: false, // 时间线页面开关
-		albums: true, // 相册页面开关
+		albums: false, // 相册页面开关
 		devices: false, // 设备页面开关
 	},
 
